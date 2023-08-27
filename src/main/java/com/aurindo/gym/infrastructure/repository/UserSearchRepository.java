36f1b6dacbef71c038a0e0c3caed9b4bcd07c182
Aurindo Jorge <ajorge@squaretrade.com>
2023-08-27 02:55:48 -0300

    Adjusting HATEAOS

---
+++ src/main/java/com/aurindo/gym/infrastructure/repository/UserSearchRepository.java
@@ -0,0 +1,14 @@
+package com.aurindo.gym.infrastructure.repository;
+
+import com.aurindo.gym.domain.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserSearchRepository extends PagingAndSortingRepository<User, String> {
+
+    Page<User> findAllByOrderByNameAsc(Pageable pageable);
+
+}
